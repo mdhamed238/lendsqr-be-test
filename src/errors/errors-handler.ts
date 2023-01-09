20967662cdd12a1c3944a07d4f8ed238696930dc
drsimplegraffiti <abayomiogunnusi@gmail.com>
2023-01-09 19:37:43 +0100

    Chores: added tsc

--- src/errors/errors-handler.ts
+++ src/errors/errors-handler.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export function notFound(req: Request, res: Response, next: NextFunction) {
     res.status(404);
-    const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+    const error = new Error(`🔍 - .Not Found - ${req.originalUrl}`);
     next(error);
 }
 
