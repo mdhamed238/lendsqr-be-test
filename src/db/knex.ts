31c22c81b1a6481d85be71b3559e9e4373302b24
drsimplegraffiti <abayomiogunnusi@gmail.com>
2023-01-10 17:29:11 +0100

    Chores: fixed prettier..

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -7,10 +7,10 @@ let knex = require('knex');
 let connection = knex(envConfig);
 
 
-// log connected to database on console
-connection.raw('select 1+1 as result').then(() => {
-    logger.info('Connected to database');
-});
+// // log connected to database on console
+// connection.raw('select 1+1 as result').then(() => {
+//     logger.info('Connected to database');
+// });
 
 export default connection;
 
